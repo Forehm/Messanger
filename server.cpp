00bb220c6a19c0b2092dcc94e4bae6968b5b6c1a
Egor <112934317+Forehm@users.noreply.github.com>
2023-04-04 15:47:14 +0300

    Update server.cpp

--- server.cpp
+++ server.cpp
@@ -3,9 +3,9 @@
 #include <WinSock2.h>
 #include <vector>
 #include <algorithm>
-#include <list>
 #include <map>
 #include <deque>
+#include <set>
 
 using namespace std;
 
@@ -43,27 +43,42 @@ public:
 		}
 		string profile_name;
 		cin >> profile_name;
-		
+
 		AddUser(login, password, profile_name);
 	}
 
-	
+
 
 
 
 private:
 	static int id;
-	list<User> all_users_;
-	map<int, list<string>> messages_history_;
-
+	deque<User> all_users_;
+	map<pair<int, int>, deque<string>> messages_storage_;
+	
 	void AddUser(const string& login, const string& password, const string& profile_name)
 	{
-		all_users_.push_back({ ++id, login, password, profile_name});
+		all_users_.push_back({ ++id, login, password, profile_name });
 	}
 
 	bool is_password_appropriate(const string& password) const
 	{
 		return password.size() >= 8 ? true : false;
+	}
+
+	void AddMessage(const User& sender, const User& receiver, const string& message)
+	{
+		int first_id = -1, second_id = -1;
+		first_id = sender.id; 
+		second_id = receiver.id;
+
+		if (first_id > second_id)
+		{
+			swap(first_id, second_id);
+		}
+
+		messages_storage_[{first_id, second_id}].push_back(message);
+		
 	}
 
 };
