8f1eeba81b136f35b7a23aeeee249caf52ac5142
Egor <112934317+Forehm@users.noreply.github.com>
2023-04-05 13:00:36 +0300

    Update Tests.h

--- Tests.h
+++ Tests.h
@@ -1,1 +1,30 @@
+#pragma once
+#include <iostream>
+#include <string>
 
+template<typename T, typename T1>
+void TestingFunction(const T& first, const T1& second, const std::string& file,
+	const std::string& function, const int& line)
+{
+	if (first != second)
+	{
+		cerr << "Error: " << file << ", " << function << ", " << line << ", " << first << " != " << second << endl;
+	}
+}
+
+#define ASSERT(real_argument, expected_argument) TestingFunction(real_argument, expected_argument, __FILE__, __FUNCTION__, __LINE__)
+
+bool TestAddUser()
+{
+
+
+}
+
+
+
+
+
+void TestServer()
+{
+	
+}
