03917a3d629be531f623fa460083aa5910b266ab
Egor <112934317+Forehm@users.noreply.github.com>
2023-04-06 22:13:28 +0300

    Update Tests.h

--- Tests.h
+++ Tests.h
@@ -2,14 +2,12 @@
 #include <iostream>
 #include <string>
 
-template<typename T, typename T1>
-void TestingFunction(const T& first, const T1& second, const std::string& file,
-	const std::string& function, const int& line)
+template<typename Function>
+void TestingFunction(Function func, const string& func_name, const int& line)
 {
-	if (first != second)
-	{
-		cerr << "Error: " << file << ", " << function << ", " << line << ", " << first << " != " << second << endl;
-	}
+	func();
+	cerr << func_name << '(' << line << ')' << " OK" << endl;
+	
 }
 
 #define ASSERT(real_argument, expected_argument) TestingFunction(real_argument, expected_argument, __FILE__, __FUNCTION__, __LINE__)
