a9ded9e9b1518052269ea1583a59d1e10a81353c
Egor <112934317+Forehm@users.noreply.github.com>
2023-04-06 22:20:42 +0300

    Update Tests.h

--- Tests.h
+++ Tests.h
@@ -12,8 +12,21 @@ void TestingFunction(Function func, const string& func_name, const int& line)
 	
 }
 
+
+template<typename T, typename T2>
+void TestingFunction(const T& real_arg, const T2& expected_arg, const string& file, const string& function,
+	const int& line)
+{
+	if (real_arg != expected_arg)
+	{
+		cerr << "Error " << file << "__" << function << "__" << line << endl;
+		abort();
+	}
+}
+
 #define ASSERT(real_argument, expected_argument) TestingFunction(real_argument, expected_argument, __FILE__, __FUNCTION__, __LINE__)
 
+#define RUN_TEST(function)TestStartingFunction(function, (#function), __LINE__)
 bool TestAddUser()
 {
 
