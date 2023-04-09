4fc362418f2544cb2fc4646eed421b8f64d4869d
Forehm <egortsepenev@icloud.com>
2023-04-09 17:04:15 +0300

    Added the socket initialization

--- Messanger.cpp
+++ Messanger.cpp
@@ -1,8 +1,11 @@
-﻿#include <iostream>
+﻿#pragma comment(lib, "ws2_32.lib")
+#include <iostream>
 #include <string>
 #include <WinSock2.h>
-#include <vector>
+#include <WS2tcpip.h>
+#include <stdio.h>
 #include <algorithm>
+#include <vector>
 #include <map>
 #include <deque>
 #include <set>
@@ -15,5 +18,34 @@ using namespace std;
 int main()
 {
 	TestServer();
+
+	WSADATA wsData;
+	WORD dll_version = MAKEWORD(2, 2);
+
+	int last_error = WSAStartup(dll_version, &wsData);
+
+
+	if (last_error == 0)
+	{
+		cerr << "WinSock initialization is OK" << endl;
+	}
+	else
+	{
+		cerr << "Error::" << WSAGetLastError() << endl;
+	}
+
+	SOCKET my_socket = socket(AF_INET, SOCK_STREAM, 0);
+
+	if (my_socket == INVALID_SOCKET)
+	{
+		cerr << "Error::" << WSAGetLastError() << endl;
+		closesocket(my_socket);
+		WSACleanup();
+		return 1;
+	}
+	else
+	{
+		cerr << "Server socket initialization is OK" << endl;
+	}
 
 }
