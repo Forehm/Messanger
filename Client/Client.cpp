683771384f28d9faf23d15619d23703d858d2aa2
Forehm <egortsepenev@icloud.com>
2023-04-10 22:36:35 +0300

    Added client code

---
+++ Client/Client.cpp
@@ -0,0 +1,29 @@
+#pragma comment(lib, "ws2_32.lib")
+#include <iostream>
+#include <string>
+#include <WinSock2.h>
+#include <WS2tcpip.h>
+#include <stdio.h>
+#include <vector>
+
+using namespace std;
+
+
+
+
+
+
+
+
+
+
+
+
+int main()
+{
+
+
+
+
+
+}
