1115e2dcde01fd5e3d59da62eb00ea171abdf6b5
Forehm <egortsepenev@icloud.com>
2023-04-10 22:58:32 +0300

    Set up the adress of the server to the client app

--- Client/Client.cpp
+++ Client/Client.cpp
@@ -10,20 +10,71 @@ using namespace std;
 
 
 
-
-
-
-
-
-
-
-
-
 int main()
 {
+	WSAData WsData;
+	WORD dll_version = MAKEWORD(2, 2);
 
+	int last_error = WSAStartup(dll_version, &WsData);
 
+	if (last_error != 0)
+	{
+		cerr << "Error:: " << WSAGetLastError() << endl;
+		return 1;
+	}
+	else
+	{
+		cerr << "WinSock initialization is OK" << endl;
+	}
 
+	SOCKET client_socket = socket(AF_INET, SOCK_STREAM, 0);
 
+	if (client_socket == INVALID_SOCKET)
+	{
+		cerr << "Error with initializing the socket::" << WSAGetLastError() << endl;
+		closesocket(client_socket);
+		WSACleanup();
+		return 1;
+	}
+	else
+	{
+		cerr << "Socket initializing is OK" << endl;
+	}
+
+	sockaddr_in server_info;
+
+	ZeroMemory(&server_info, sizeof(server_info));
+
+	in_addr adress;
+	last_error = inet_pton(AF_INET, "192.168.50.121", &adress);
+	if (last_error != 0)
+	{
+		cerr << "Convertion ipv4 adress failed, error::" << WSAGetLastError() << endl;
+		closesocket(client_socket);
+		WSACleanup();
+		return 1;
+	}
+	else
+	{
+		cerr << "Convertion ipv4 is OK" << endl;
+	}
+
+	server_info.sin_family = AF_INET;
+	server_info.sin_addr = adress;
+	server_info.sin_port = htons(1234);
+
+	last_error = connect(client_socket, (sockaddr*)&server_info, sizeof(server_info));
+
+	if (last_error != 0)
+	{
+		cerr << "Connection to server failed, error::" << WSAGetLastError() << endl;
+		closesocket(client_socket);
+		WSACleanup();
+		return 1;
+	}
+	else
+	{
+		cerr << "Connection is set up :)" << endl;
+	}
 
 }
